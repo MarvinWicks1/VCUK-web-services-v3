b10eba75dac1fe57bcf12e1b899e6721f252a3a8
MarvinWicks1 <marvinwicks238@gmail.com>
2026-07-05 10:19:25 +0100

    Support smaller archive repair chunks

--- unpack.mjs
+++ unpack.mjs
@@ -1,8 +1,21 @@
 import { readdirSync, readFileSync, writeFileSync, rmSync } from 'node:fs';
 import { execFileSync } from 'node:child_process';
 
-const parts = readdirSync('.').filter((name) => /^site\.part\.\d+$/.test(name)).sort();
-const base64 = parts.map((part) => readFileSync(part, 'utf8')).join('');
+const files = readdirSync('.');
+const partNames = files.filter((name) => /^site\.part\.\d+$/.test(name)).sort();
+
+const base64 = partNames.map((part) => {
+  const subParts = files
+    .filter((name) => name.startsWith(`${part}.`))
+    .sort();
+
+  if (subParts.length > 0) {
+    return subParts.map((subPart) => readFileSync(subPart, 'utf8')).join('');
+  }
+
+  return readFileSync(part, 'utf8');
+}).join('');
+
 writeFileSync('/tmp/vcuk-site.tar.gz', Buffer.from(base64, 'base64'));
 execFileSync('tar', ['-xzf', '/tmp/vcuk-site.tar.gz'], { stdio: 'inherit' });
 rmSync('/tmp/vcuk-site.tar.gz', { force: true });
